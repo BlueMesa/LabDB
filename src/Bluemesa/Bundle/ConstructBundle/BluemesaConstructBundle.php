4299c3181d01d3555feb760b7762d0ae6ccedb6f
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2016-09-29 13:51:07 +0200

    Moving CRUD and Construct bundles into own repos

--- src/Bluemesa/Bundle/ConstructBundle/BluemesaConstructBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bluemesa\Bundle\ConstructBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BluemesaConstructBundle extends Bundle
-{
-}
--- src/Bluemesa/Bundle/CrudBundle/BluemesaCrudBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bluemesa\Bundle\CrudBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BluemesaCrudBundle extends Bundle
-{
-}
