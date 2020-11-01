dd7380ce72cf304d7b822dbdfec315a81ea4c0b4
root <root@sfl-labdb.int.vibfolks.eu>
2020-11-01 15:20:11 +0000

    Local changes

---
+++ src/Bluemesa/CriImapUserBundle/BluemesaCriImapUserBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the BluemesaCriImapUserBundle.
+ *
+ * Copyright (c) 2016 BlueMesa LabDB Contributors <labdb@bluemesa.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bluemesa\CriImapUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BluemesaCriImapUserBundle extends Bundle
+{
+}
